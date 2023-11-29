de6d1309c55d8e9d94e0e2ef79fdbcbef67257d2
ebeetiobhio <ebe.etiobhio@and.digital>
2023-11-29 20:05:05 +0000

    navbar styling

--- blue-project/app/components/navbar.js
+++ blue-project/app/components/navbar.js
@@ -1,4 +1,13 @@
+"use client";
+
 import Link from "next/link";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  background: #333;
+  color: #fff;
+  padding: 1rem;
+`;
 
 const Navbar = () => (
   <nav>
