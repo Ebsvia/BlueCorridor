da4ef202e0aca1cb206f8d4003cc196eee98ce9e
ebeetiobhio <ebe.etiobhio@and.digital>
2023-11-30 07:32:41 +0000

    added profile page

--- blue-project/components/navbar.js
+++ blue-project/components/navbar.js
@@ -1,7 +1,26 @@
 import Link from "next/link";
 
 const Navbar = () => (
-  <nav>
+  <nav style={{ display: "flex", justifyContent: "space-between" }}>
+    <style jsx>{`
+      nav {
+        display: flex;
+        justify-content: space-between;
+        padding: 1em;
+        background-color: #d8d8d8;
+        color: #fff;
+      }
+      ul {
+        list-style: none;
+        display: flex;
+        justify-content: space-around;
+        width: 30%;
+      }
+      li a {
+        color: #000000;
+        text-decoration: none;
+      }
+    `}</style>
     <ul>
       <li>
         <Link href="/">Home</Link>
@@ -12,7 +31,11 @@ const Navbar = () => (
       <li>
         <Link href="/contact">Contact</Link>
       </li>
+      <li></li>
     </ul>
+    <Link href="/profile">
+      <button className="button-class">Create a profile</button>
+    </Link>
   </nav>
 );
 
---
+++ blue-project/pages/profile.js
@@ -0,0 +1,11 @@
+import Navbar from "@/navbar";
+
+const Profile = () => (
+  <div>
+    <Navbar />
+    <h1>Your Profile</h1>
+    <p>Create a profile</p>
+  </div>
+);
+
+export default Profile;
